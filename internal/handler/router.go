86bad538501165097abb3ac494a8526215485e12
orvice L <orvice@gmail.com>
2025-07-16 00:54:34 +0800

    Add request URL modification in reverse proxy initialization
    - Updated the proxy director function to set the request URL host and scheme based on the upstream configuration.

--- internal/handler/router.go
+++ internal/handler/router.go
@@ -26,6 +26,10 @@ func Init() error {
 			return err
 		}
 		proxy := httputil.NewSingleHostReverseProxy(upstreamURL)
+		proxy.Director = func(req *http.Request) {
+			req.URL.Host = proxyConf.Host
+			req.URL.Scheme = upstreamURL.Scheme
+		}
 		reserveProxy[proxyConf.Host] = proxy
 	}
 	return nil
